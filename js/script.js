a2412e0057a4abdf5a3db48c25184cbcfc1ad1e2
nataliaaraujo0 <81394067+nataliaaraujo0@users.noreply.github.com>
2021-03-29 18:18:22 -0300

    novoindex

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
 //script ser axecutado assim que a página estiver carregada
 window.addEventListener('load',start);
+//variavel global para armazenar nomes
+var globalNames = ['um','dois','tres'];
+
 //função de start
-function start(){
-    console.log('start');
-    
+function start() {
+    //funcão para não recarregar a pagina ao add no form
+    preventFormSubmit();
+
+}  
+
+//criar funcao prevent
+function preventFormSubmit () {
+
+    //impelentar fuction
+    function handleFormSubmit(event){
+    //evitar que a pagina seja recarregada
+    event.preventDefault();
+    }
+
+    //referenciar o form
+    const form = document.querySelector('form');
+    //adc evento no form
+    form.addEventListener('submit', handleFormSubmit);
 }
