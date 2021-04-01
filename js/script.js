535f66452c8abadf7e75f00eeeea468d1d0f1318
nataliaaraujo0 <81394067+nataliaaraujo0@users.noreply.github.com>
2021-04-01 00:49:52 -0300

    new index

--- js/script.js
+++ js/script.js
@@ -1,26 +1,73 @@
 //script ser axecutado assim que a página estiver carregada
 window.addEventListener('load',start);
 //variavel global para armazenar nomes
-var globalNames = ['um','dois','tres'];
+var globalComent = ['um','dois','tres','quatro'];
 
 //função de start
 function start() {
+    
+    inputComent = document.querySelector('#inputComent');   //AQUI
     //funcão para não recarregar a pagina ao add no form
     preventFormSubmit();
+    activateInput();
+  
+ 
 
 }  
 
+
 //criar funcao prevent
-function preventFormSubmit () {
+function preventFormSubmit () { 
+    function handleFormSubmit(event){ //impelentar fuction
+     event.preventDefault();  //evitar que a pagina seja recarregada
+    } 
 
-    //impelentar fuction
-    function handleFormSubmit(event){
-    //evitar que a pagina seja recarregada
-    event.preventDefault();
+    var form = document.querySelector('form');  //referenciar o form
+     form.addEventListener('submit', handleFormSubmit);    //adc evento no form
+}  
+
+
+
+function activateInput(){
+    function insertComent(newComent){ //adc novo coomentário
+        globalComent.push(newComent); 
+        render(); 
+            console.log(globalComent);
+         
     }
+//capturar texto quando usuário pressionar a tecla "ENTER"
+function  handleTyping(event){
+    if (event.key === 'Enter') {
+         insertComent(event.target.value); //inserir nome
+        }
+      }
 
-    //referenciar o form
-    const form = document.querySelector('form');
-    //adc evento no form
-    form.addEventListener('submit', handleFormSubmit);
+    //monitorar digitação
+     inputComent.addEventListener('keyup', handleTyping);
+
+} 
+
+//montar lista - renderizar comentários
+function render(){
+    var divComents = document.querySelector('#coments');
+     divComents.innerHTML = '';   //limpar a div
+
+    var ul = document.createElement('ul');
+
+    for(var i = 0; i < globalComent.length; i++) {
+        var currentComent = globalComent[i];
+
+        var li = document.createElement('li');
+        var button = document.createElement('button');
+        button.textContent = 'x';
+        li.textContent = currentComent;
+        ul.appendChild(li);
+    }
+    divComents.appendChild(ul);
+    clearInput();
+}
+
+//limpar campo depois de comentar
+function clearInput(){
+    inputComent.value = '';
 }
